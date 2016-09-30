e504ff5d5194cb922c2476940c46a742feecd261
Aviv Levinsky <hello@avivlevinsky.com>
2016-10-01 01:31:06 +0300

    config simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,16 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'webmock/rspec'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  else
+    SimpleCov.start 'rails'
+  end
+
   config.include FactoryGirl::Syntax::Methods
 
   config.use_transactional_fixtures = true
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
+require 'simplecov'
+require 'webmock/rspec'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
   end
 
   config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
     mocks.verify_partial_doubles = true
   end
 
