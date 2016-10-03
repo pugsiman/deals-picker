0ebebe38195d23887d5b5b27a1c160b03a83492f
Aviv Levinsky <hello@avivlevinsky.com>
2016-10-04 00:26:53 +0300

    update specs

---
+++ spec/controllers/product_name_searches_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe ProductNameSearchesController, type: :controller do
+
+end
---
+++ spec/services/product_name/search_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ProductName::Search do
+  describe '.call' do
+    context 'results found' do
+      it 'returns product name candidates' do
+        VCR.use_cassette 'product_name/search/results_found' do
+          expect(described_class.('iron rangers')).to eq [
+            "Red Wing Heritage Iron Ranger 6\" Boot",
+            "Wolverine 1000 Mile Men's Wolverine 1000 Mile Boots",
+            "Chippewa Men's 6\" Rugged Handcrafted Lace-Up Boot"
+          ]
+        end
+      end
+    end
+
+    context 'result not found' do
+      it 'returns error' do
+        VCR.use_cassette 'product_name/search/results_not_found' do
+          expect(described_class.('ahsdkjashdkajsdhaksjdhasjkhd')).to eq []
+        end
+      end
+    end
+  end
+end
