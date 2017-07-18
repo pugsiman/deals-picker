6ee714af84552792517e1874779db342e72f2779
Aviv Levinsky <hello@avivlevinsky.com>
2017-07-19 02:03:29 +0300

    remove debugger

--- app/services/search_product_names.rb
+++ app/services/search_product_names.rb
@@ -11,7 +11,6 @@ class SearchProductNames
   def call
     http_client = SetClient.call
     searchables.flat_map do |searchable|
-      binding.pry
       page = http_client.get(searchable[:url] + search_value)
       extractor = Extractors::ProductNamesFromSearch.create(searchable[:platform])
       extractor.extract(page).first(3)
